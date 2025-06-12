dd4a06bca73c2192c0411788b5fe554b1e3fe92a
FollyMaddy <meulenf@gmx.net>
2025-06-12 10:23:31 +0200

    wip
    - get newer cmake and use this when compiling
    - use different forks for different platforms (aarch (arm64) and x86_64)
    - remove obsolete commands
    
    script has not been fully tested fully !

--- scriptmodules/emulators/borked3ds.sh
+++ scriptmodules/emulators/borked3ds.sh
@@ -5,6 +5,7 @@
 #
 # https://github.com/RetroPie/RetroPie-Setup
 # https://github.com/Exarkuniv/RetroPie-Extra
+# https://github.com/FollyMaddy/RetroPie-Share
 #
 # See the LICENSE file distributed with this source and at
 # https://raw.githubusercontent.com/Exarkuniv/RetroPie-Extra/master/LICENSE
@@ -24,43 +25,59 @@ function depends_borked3ds() {
     fi
  
     # Additional libraries required for running
-#local depends=(build-essential cmake clang clang-format libc++-dev libsdl2-dev libssl-dev qt6-l10n-tools qt6-tools-dev qt6-tools-dev-tools  qt6-base-dev qt6-base-private-dev libxcb-cursor-dev libvulkan-dev qt6-multimedia-dev libqt6sql6 libqt6core6 libasound2-dev xorg-dev libx11-dev libxext-dev libpipewire-0.3-dev libsndio-dev libfdk-aac-dev ffmpeg libgl-dev libswscale-dev libavformat-dev libavcodec-dev libavdevice-dev libglut3.12 libglut-dev freeglut3-dev mesa-vulkan-drivers libinput-dev) 
- local depends=(build-essential cmake clang clang-format libc++-dev libsdl2-dev libssl-dev qt6-l10n-tools qt6-tools-dev qt6-tools-dev-tools  qt6-base-dev qt6-base-private-dev libxcb-cursor-dev libvulkan-dev qt6-multimedia-dev libqt6sql6 libqt6core6 libasound2-dev xorg-dev libx11-dev libxext-dev libpipewire-0.3-dev libsndio-dev libfdk-aac-dev ffmpeg libgl-dev  libswscale-dev libavformat-dev libavcodec-dev libavdevice-dev libglut3.12 libglut-dev freeglut3-dev mesa-vulkan-drivers) 
-
-		getDepends "${depends[@]}"
+	#local depends=(build-essential cmake clang clang-format libc++-dev libsdl2-dev libssl-dev qt6-l10n-tools qt6-tools-dev qt6-tools-dev-tools  qt6-base-dev qt6-base-private-dev libxcb-cursor-dev libvulkan-dev qt6-multimedia-dev libqt6sql6 libqt6core6 libasound2-dev xorg-dev libx11-dev libxext-dev libpipewire-0.3-dev libsndio-dev libfdk-aac-dev ffmpeg libgl-dev libswscale-dev libavformat-dev libavcodec-dev libavdevice-dev libglut3.12 libglut-dev freeglut3-dev mesa-vulkan-drivers libinput-dev) 
+	if isPlatform "rpi" && isPlatform "64bit"; then
+		local depends=(build-essential cmake clang clang-format libc++-dev libsdl2-dev libssl-dev qt6-l10n-tools qt6-tools-dev qt6-tools-dev-tools  qt6-base-dev qt6-base-private-dev libxcb-cursor-dev libvulkan-dev qt6-multimedia-dev libqt6sql6 libqt6core6 libasound2-dev xorg-dev libx11-dev libxext-dev libpipewire-0.3-dev libsndio-dev libfdk-aac-dev ffmpeg libgl-dev  libswscale-dev libavformat-dev libavcodec-dev libavdevice-dev libglut3.12 libglut-dev freeglut3-dev mesa-vulkan-drivers robin-map-dev) 
+	else
+		# packages not in bookworm for x86_64 : libfdk-aac-dev
+		# robin-map-dev is in the source and found when using https://github.com/borked3ds/Borked3DS.git
+		local depends=(build-essential cmake clang clang-format libc++-dev libsdl2-dev libssl-dev qt6-l10n-tools qt6-tools-dev qt6-tools-dev-tools  qt6-base-dev qt6-base-private-dev libxcb-cursor-dev libvulkan-dev qt6-multimedia-dev libqt6sql6 libqt6core6 libasound2-dev xorg-dev libx11-dev libxext-dev libpipewire-0.3-dev libsndio-dev ffmpeg libgl-dev  libswscale-dev libavformat-dev libavcodec-dev libavdevice-dev libglut3.12 libglut-dev freeglut3-dev mesa-vulkan-drivers) 
+	fi
+	getDepends "${depends[@]}"
  
 }
  # openssl
 function sources_borked3ds() {
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-regression 
-   #gitPullOrClone "$md_build" https://github.com/borked3ds/Borked3DS.git
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vulkan-validation
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git mobile-gpus
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git gpu-revert
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git opengles-dev
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git opengles-dev
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-mem-alloc
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-0
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-1
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-2
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-3
-   #gitPullOrClone "$md_build" https://github.com/Borked3DS/Borked3DS.git
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git opengles-dev-v2
-   #gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git fix-gcc12
-   gitPullOrClone "$md_build" https://github.com/gvx64/Borked3DS-rpi.git
-   
+#backup of all forks, replace in if function when needed
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-regression 
+#gitPullOrClone "$md_build" https://github.com/borked3ds/Borked3DS.git
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vulkan-validation
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git mobile-gpus
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git gpu-revert
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git opengles-dev
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git opengles-dev
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-mem-alloc
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-0
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-1
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-2
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git vk-revert-3
+#gitPullOrClone "$md_build" https://github.com/Borked3DS/Borked3DS.git
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git opengles-dev-v2
+#gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git fix-gcc12
+#gitPullOrClone "$md_build" https://github.com/gvx64/Borked3DS-rpi.git
+
+	#Borked3DS requires a cmake 3.5 as minimum, we will use the 4.0.2 binary
+	#find the files on "https://cmake.org/files/v4.0/" (cmake-4.0.2.tar.gz is source only)
+	
+	if isPlatform "aarch64"; then
+		downloadAndExtract https://cmake.org/files/v4.0/cmake--linux-aarch64.tar.gz "$md_build"
+		mv cmake-4.0.2* cmake-4.0.2
+		gitPullOrClone "$md_build" https://github.com/gvx64/Borked3DS-rpi.git
+	else
+		downloadAndExtract https://cmake.org/files/v4.0/cmake-4.0.2-linux-x86_64.tar.gz "$md_build"
+		mv cmake-4.0.2* cmake-4.0.2
+		gitPullOrClone "$md_build" https://github.com/rtiangha/Borked3DS.git
+	fi	
 }
  
 function build_borked3ds() {
-    cd "$md_build/borked3ds"
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . -- -j"$(nproc)"
+	mkdir build
+	cd build
+	$md_build/cmake-4.0.2/bin/cmake .. -DCMAKE_BUILD_TYPE=Release
+	$md_build/cmake-4.0.2/bin/cmake --build . -- -j"$(nproc)"
     md_ret_require="$md_build/build/bin"
- 
 }
  
 function install_borked3ds() {
@@ -69,14 +86,10 @@ function install_borked3ds() {
       #'build/bin/Release/borked3ds-cli'
 	  #'build/bin/Release/borked3ds-room'
 	  #'build/bin/Release/tests'
- 
-	        ''
       )
- 
 }
  
 function configure_borked3ds() {
- 
     mkRomDir "3ds"
     ensureSystemretroconfig "3ds"
     local launch_prefix
@@ -86,7 +99,5 @@ function configure_borked3ds() {
 	#addEmulator 1 "$md_id-room" "3ds" "$launch_prefix$md_inst/borked3ds-room"
 	#addEmulator 2 "$md_id-cli" "3ds" "$launch_prefix$md_inst/borked3ds-cli"
 	#addEmulator 3 "$md_id-tests" "3ds" "$launch_prefix$md_inst/tests"
-    addSystem "3ds" "3ds" ".3ds .3dsx .elf .axf .cci ,cxi .app"
- 
- 
+    addSystem "3ds" "3ds" ".3ds .3dsx .elf .axf .cci ,cxi .app" 
 }
